35820324861322b39b3bf66e9447a75e1c298363
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-05-15 12:57:48 +0530

    Connected DB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
+// Importing dotenv to load environment variables
 dotenv.config();
 
+// Initialize express app
 const app = express();
 
+// Database connection
+connectDB();
+
 const PORT = process.env.PORT || 3000;
+
+// Middleware
 app.use(express.json());
 
 app.listen(PORT, () => {
