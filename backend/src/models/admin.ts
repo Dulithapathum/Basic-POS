7cc91de41cbfa4f17f1a78fd156a8a7e747079e2
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-05-15 14:32:34 +0530

    Created Admin Model

---
+++ backend/src/models/admin.ts
@@ -0,0 +1,16 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IAdmin extends Document {
+  email: string;
+  password: string;
+}
+
+const AdminSchema = new Schema<IAdmin>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Admin = model<IAdmin>("Admin", AdminSchema);
