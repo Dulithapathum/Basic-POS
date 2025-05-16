8e79f393d1857672c7721be5c546071f4499a742
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-05-16 09:24:40 +0530

    Configured Customer API

--- backend/src/controller/customerController.ts
+++ backend/src/controller/customerController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import { custom } from "zod";
 
 import { Customer } from "../models/Customer";
 import { customerValidator } from "../validators/customerValidator";
 
+// Customer Add
 export const addCustomer = async (req: Request, res: Response) => {
   try {
     const validateData = customerValidator.safeParse(req.body);
@@ -20,5 +20,49 @@ export const addCustomer = async (req: Request, res: Response) => {
     }
   } catch (error) {
     res.status(500).json({ message: "Error adding customer", error });
+  }
+};
+
+// Customer Delete
+export const deleteCustomer = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const customer = await Customer.findByIdAndDelete(id);
+    if (customer) {
+      res.status(200).json({ message: "Customer deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Customer not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting customer", error });
+  }
+};
+
+// Get All Customers
+export const getAllCustomer = async (req: Request, res: Response) => {
+  try {
+    const customers = await Customer.find();
+    if (!customers) {
+      res.status(404).json({ message: "Customer not found" });
+    } else {
+      res.status(200).json({ success: true, data: customers });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving customers", error });
+  }
+};
+
+// Get Customers by id
+export const getCustomerById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const customer = await Customer.findById(id);
+    if (!customer) {
+      res.status(404).json({ message: "Customer Not Found" });
+    } else {
+      res.status(200).json({ success: true, customer });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving customer", error });
   }
 };
--- backend/src/routes/customerRoutes.ts
+++ backend/src/routes/customerRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { addCustomer } from "../controller/customerController";
+import {
+  addCustomer,
+  deleteCustomer,
+  getAllCustomer,
+  getCustomerById,
+} from "../controller/customerController";
 
 const router = Router();
 
 router.post("/add", addCustomer);
+router.delete("/:id", deleteCustomer);
+router.get("/", getAllCustomer);
+router.get("/:id", getCustomerById);
 
 export default router;
