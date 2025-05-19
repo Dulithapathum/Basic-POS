073a5dac2b48236ace5fc4e946bed2e7375a6804
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-05-19 12:18:09 +0530

    Confegure Admin Login

--- frontend/src/pages/AdminLogin.tsx
+++ frontend/src/pages/AdminLogin.tsx
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { showToast } from "../utils/toast";
 
 const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -28,8 +30,20 @@ const AdminLogin = () => {
     setShowPassword(!showpassword);
   };
 
-  const onSubmit = () => {};
+  const onSubmit = async (data: LoginFormInputs) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/admin/login`,
+        data
+      );
 
+      localStorage.setItem("token", response.data.token);
+      showToast("success", "Login successful");
+      navigate("/");
+    } catch (error: any) {
+      showToast("error", error.response?.data?.message || error.message);
+    }
+  };
   return (
     <div className="w-full h-screen mx-auto flex items-center justify-center bg-[#efefef]">
       <div className="max-w-4xl m-3 sm:m-0 flex flex-col sm:flex-row items-center bg-[#FFC903] rounded-lg">
